d1cd8285cdcbe740bacf6064b9b43db7cff2a04a
Sergio Vera <sergio.vera@icloud.com>
2021-08-18 15:11:00 +0000

    implementing fork

--- ConcptExeShell/path.c
+++ ConcptExeShell/path.c
@@ -2,36 +2,43 @@
 
 int main(int ac, char **av, char **env)
 {
+	(void)ac;
 	int i;
 	size_t n;
 	char *path;
 	char *token;
-	char *inputBuffer = NULL;
-	(void)ac;
+	char *inputBuffer;
 	size_t inputLen;
+	struct stat statBuffer;
+	int status;
 	int interactive = 1;
-	
+	pid_t pid;
+
 	token = malloc(sizeof(char *) * 10);
 
 	if (isatty(STDIN_FILENO) == 0)
 	{
 		interactive = 0;
+		return (1);
 	}
-	while (av[0] != NULL)
+	if (av[0])
 	{
-		if(interactive == 1)
+		if (interactive == 1)
 			printf("$Hell>");
 
 		inputLen = getline(&inputBuffer, &n, stdin);
 		if (inputLen == -1)
+		{
 			return (1);
+		}
 		printf("%s", inputBuffer);
-		if (av[1] != av[0])
-		{	/* TOKENIZER*/
+		
+		if (av[0] && av[1])
+		/* TOKENIZER*/
 			interactive = 0;
 			for (i = 0; env[i] != NULL; i++)
 			{
-				if (strncmp(env[i], "PATH", 4) == 0);
+				if (strncmp(env[i], "PATH", 4));
 				{
 					printf("%s\n", env[i]);
 				}
@@ -43,10 +50,25 @@ int main(int ac, char **av, char **env)
 			{
 
 				token = strtok(NULL, ":");
-				printf("%s\n", env[i]);
+				printf("Path: %s\n", token);
 			}
+			i++;
+		
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("Error");
+			return (1);
 		}
+		if (pid == 0)
+		{
+			/*execve(token[0], token, NULL);*/
+			wait(&status);
+		}	
 	}
-	free(token);
+	printf("PID: %d\n", pid);
+	printf("PATH = %s\n", path);
+	printf("ENV: %s\n", env[i]);
+
 	return (0);
 }
